277c6527fa510b8eef4b2c9ed2bd326ef1b7f025
sebastian <12151772+bestiasan@users.noreply.github.com>
2021-10-23 16:01:54 +0200

    add info

--- active_directory_pass.py
+++ active_directory_pass.py
@@ -1,16 +1,22 @@
+"""
+Based on :
+https://stackoverflow.com/questions/30586604/correct-encoding-for-unicodepwd-in-active-directory
+https://ldapwiki.com/wiki/Passwords%20Using%20LDIF
+"""
+
 import base64
 
 ENCODING = 'utf-16le'
 
 
-def code_pass(password: str):
+def unicode_pwd(password: str):
     _pass_encoded = password.encode(ENCODING)
     _pass_coded = base64.b64encode(_pass_encoded)
     print(f'{password} -> {_pass_encoded} -> {_pass_coded}')
     return _pass_coded
 
 
-def decode_pass(password_c):
+def deunicode_pwd(password_c):
     _pass_decoded = base64.b64decode(password_c)
     _pass = _pass_decoded.decode(ENCODING)
     print(f'{password_c} -> {_pass_decoded} -> {_pass}')
@@ -18,12 +24,10 @@ def decode_pass(password_c):
 
 
 def test_pass(password):
+    cp = unicode_pwd(password)
+    assert password == deunicode_pwd(cp)
 
-    cp = code_pass(password)
-    assert password == decode_pass(cp)
 
-
-test_pass('"newPassword"')
-test_pass('dasdj#W$RWEKLRJ$')
-
-pass
+if __name__ == '__main__':
+    test_pass('"newPassword"')
+    test_pass('"dasdj#W$RWEKLRJ$"')
